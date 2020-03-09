e5ff424edd840227b7aee575a2a85f729560e044
AlphaTec <juho3471@gmail.com>
2020-03-09 16:56:45 +0900

    solve day4_4873 problem

--- Intermediate Course/day4_4873.py
+++ Intermediate Course/day4_4873.py
@@ -1,29 +1,15 @@
 T = int(input())
 for t in range(T):
-    L = []
+    string = list(input())
+    ans = []
 
-    for i in input():
-        L.append(i)
+    for char in string:
+        if ans == []:
+            ans.append(char)
+            continue
+        if char == ans[-1]:
+            ans.pop(-1)
+        elif char != ans[-1]:
+            ans.append(char)
 
-    test = L[0]
-    newL = [L[0]]
-
-    while 1:
-        for i in range(1, len(L)):
-            if L[i] != test:
-                newL.append(L[i])
-                test = L[i]
-            else:
-                newL.pop()
-                test = '_'
-        if L == newL:
-            break
-        else:
-            L = newL
-            if len(L) == 0:
-                break
-            else:
-                newL = [L[0]]
-                test = L[0]
-
-    print("#%s" % (t + 1), len(L))
+    print("#%s" % (t+1), len(ans))
